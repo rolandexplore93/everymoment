29ff69db3133deff840c51a05110e711ec37587b
rolandexplore93 <roland2rule@gmail.com>
2022-09-17 11:39:01 +0100

    Merge main branch with master branch

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1
 app.use(bodyParser.text({ limit: '200mb' }));
 app.use(cors({origin: true, credentials: true}));
 
-app.use('/', (req, res) => {
-    res.send('Backend server for everymoment application')
-})
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+    res.send("Welcome to everymoment app server")
+});
 
 const PORT = process.env.PORT || 5000;
 
