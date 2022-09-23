8aa657a9d73b1c63d81a14b369481221f81ccbb0
rolandexplore93 <roland2rule@gmail.com>
2022-09-23 22:52:22 +0100

    create profile icons inside menubar

--- client/src/components/primaryComponents/Menubar/Menubar.jsx
+++ client/src/components/primaryComponents/Menubar/Menubar.jsx
@@ -1,6 +1,7 @@
 import "./Menubar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import images from "../../../assets/images";
 
 const Menubar = () => {
   return (
@@ -8,6 +9,13 @@ const Menubar = () => {
       <div className="Menubar__title">
         <p className="Menubar__name">Share Your Moments</p>
         <FontAwesomeIcon className="Menubar__icon" icon={faCirclePlus} />
+      </div>
+      <div className="Menubar__profile">
+        <div className="Menubar__profile__account">
+          <img className="Menubar__profile__avatar" src={images.sparklingTree} alt="avater" />
+          <p className="Menubar__profile__user">RollyJS</p>
+        </div>
+        <button className="Menubar__profile__logout" type="submit">Logout</button>
       </div>
     </div>
   );
