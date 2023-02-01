49bb2286a150b486544a1e02d05d3841fec316d6
rolandexplore93 <roland2rule@gmail.com>
2023-02-01 22:54:27 +0000

    Store user login data to local storage

--- client/src/services/reducers/auth.js
+++ client/src/services/reducers/auth.js
@@ -1,10 +1,11 @@
 import { AUTH, LOGOUT } from "../../constants/actionTypes";
 
-const authReducer = (state = { auth: null }, action) => {
+const authReducer = (state = { authData: null }, action) => {
     switch (action.type) {
         case AUTH:
             console.log(action?.data)
-            return state
+            localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
+            return { ...state, authData: action?.data}
         
         default: 
             return state
