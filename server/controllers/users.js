e34a7ab43aba12f80a1c77e82031ae89140031fd
rolandexplore93 <roland2rule@gmail.com>
2023-02-07 01:21:51 +0000

    Implement sign in functionality

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import User from '../models/user.js';
+import bcyprt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+
+console.log(process.env.SECRETJWT)
 
 export const signin = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) return res.status(404).json({message: "User doesn't exist... Please Sign up!"});
+        const isPasswordCorrect = await bcyprt.compare(password, existingUser.password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid login credentials"});
+        
+        // Create a jwt token to determine the duration which a user stays login
+        const token = jsonwebtoken.sign({ email: existingUser.email, id: existingUser._id }, process.env.SECRETJWT, { expiresIn: '180secs'});
+        res.status(200).json({ message: existingUser, token})
+    } catch (error) {
+        res.status(500).json("Server error. Please, try again.")
+    }
 
 }
 
 export const signup = async () => {
+    
 
 }
